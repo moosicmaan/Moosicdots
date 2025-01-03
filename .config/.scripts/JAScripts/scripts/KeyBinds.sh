4a5d4aec4670bf2bc4a16c55dbdc70b70bb5d032
Jason Bradberry <j_bradberry@yahoo.com>
2025-01-03 15:44:55 -0700

    move user scripts around - Look at JAScripts for future ideas

--- .config/.scripts/JAScripts/scripts/KeyBinds.sh
+++ .config/.scripts/JAScripts/scripts/KeyBinds.sh
@@ -6,25 +6,24 @@
 pkill yad || true
 
 # Define the config files
-KEYBINDS_CONF="$HOME/.config/hypr/conf/keybindins.conf"
-USER_KEYBINDS_CONF="$HOME/.config/hypr/conf/keybinding.conf"
-LAPTOP_CONF="$HOME/.config/hypr/conf/keybinding.conf"
+KEYBINDS_CONF="$HOME/.config/hypr/configs/Keybinds.conf"
+USER_KEYBINDS_CONF="$HOME/.config/hypr/UserConfigs/UserKeybinds.conf"
+LAPTOP_CONF="$HOME/.config/hypr/UserConfigs/Laptop.conf"
 
 # Combine the contents of the keybinds files and filter for keybinds
 KEYBINDS=$(cat "$KEYBINDS_CONF" "$USER_KEYBINDS_CONF" | grep -E '^(bind|bindl|binde|bindm)')
 
 # Check if Laptop.conf exists and add its keybinds if present
 if [[ -f "$LAPTOP_CONF" ]]; then
-  LAPTOP_BINDS=$(grep -E '^(bind|bindl|binde|bindm)' "$LAPTOP_CONF")
-  KEYBINDS+=$'\n'"$LAPTOP_BINDS"
+    LAPTOP_BINDS=$(grep -E '^(bind|bindl|binde|bindm)' "$LAPTOP_CONF")
+    KEYBINDS+=$'\n'"$LAPTOP_BINDS"
 fi
 
 # check for any keybinds to display
 if [[ -z "$KEYBINDS" ]]; then
-  echo "No keybinds found."
-  exit 1
+    echo "No keybinds found."
+    exit 1
 fi
 
 # Use rofi to display the keybinds
-echo "$KEYBINDS" | rofi -dmenu -i -p "Keybinds" -config ~/.config/rofi/config.rasi
-
+echo "$KEYBINDS" | rofi -dmenu -i -p "Keybinds" -config ~/.config/rofi/config-keybinds.rasi
