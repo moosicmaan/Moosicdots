b58a3a3cae781628e5476e8a8e1ea88833e00763
Jason Bradberry <j_bradberry@yahoo.com>
2025-01-04 09:05:57 -0700

    cleaning up some scripts

--- .config/.scripts/MediaCtrl.sh
+++ .config/.scripts/MediaCtrl.sh
@@ -2,60 +2,68 @@
 # /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
 # Playerctl
 
-music_icon="$HOME/.config/swaync/icons/music.png"
+# music_icon="$HOME/.config/.settngs/icons/music.png"
+music_icon="/usr/share/icons/candy-icons/mimetypes/scalable/audio-x-generic.svg"
+# music_icon="music"
 
 # Play the next track
 play_next() {
-    playerctl next
-    show_music_notification
+  playerctl next
+  sleep 2
+  show_music_notification
 }
 
 # Play the previous track
 play_previous() {
-    playerctl previous
-    show_music_notification
+  playerctl previous
+  sleep 2
+  show_music_notification
 }
 
 # Toggle play/pause
 toggle_play_pause() {
-    playerctl play-pause
-    show_music_notification
+  playerctl play-pause
+  sleep 2
+  show_music_notification
 }
 
 # Stop playback
 stop_playback() {
-    playerctl stop
-    notify-send -e -u low -i "$music_icon" "Playback Stopped"
+  playerctl stop
+  sleep 2
+  notify-send -e -u low -i "$music_icon" "Playback Stopped"
 }
 
 # Display notification with song information
 show_music_notification() {
-    status=$(playerctl status)
-    if [[ "$status" == "Playing" ]]; then
-        song_title=$(playerctl metadata title)
-        song_artist=$(playerctl metadata artist)
-        notify-send -e -u low -i "$music_icon" "Now Playing:" "$song_title\nby $song_artist"
-    elif [[ "$status" == "Paused" ]]; then
-        notify-send -e -u low -i "$music_icon" "Playback Paused"
-    fi
+  status=$(playerctl status)
+  if [[ "$status" == "Playing" ]]; then
+    song_title=$(playerctl metadata title)
+    song_artist=$(playerctl metadata artist)
+    music_icon="/usr/share/icons/candy-icons/status/scalable/media-playback-playing.svg"
+    notify-send -e -u normal -i "$music_icon" "Now Playing:" "$song_title\nby $song_artist"
+  elif [[ "$status" == "Paused" ]]; then
+    music_icon="/usr/share/icons/candy-icons/status/scalable/media-playback-paused.svg"
+    notify-send -e -u normal -i "$music_icon" "Playback Paused"
+  fi
 }
 
 # Get media control action from command line argument
 case "$1" in
-    "--nxt")
-        play_next
-        ;;
-    "--prv")
-        play_previous
-        ;;
-    "--pause")
-        toggle_play_pause
-        ;;
-    "--stop")
-        stop_playback
-        ;;
-    *)
-        echo "Usage: $0 [--nxt|--prv|--pause|--stop]"
-        exit 1
-        ;;
+"--nxt")
+  play_next
+  ;;
+"--prv")
+  play_previous
+  ;;
+"--pause")
+  toggle_play_pause
+  ;;
+"--stop")
+  stop_playback
+  ;;
+*)
+  echo "Usage: $0 [--nxt|--prv|--pause|--stop]"
+  exit 1
+  ;;
 esac
