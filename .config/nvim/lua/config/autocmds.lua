4d006f9fad753f18d4dddb2970992ee99451b45b
Jason Bradberry <j_bradberry@yahoo.com>
2025-02-02 11:21:30 -0700

    icon updates and starmaker siplification

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,22 +6,29 @@
 --
 --  Close NvimTree automatically if It's the Last Window
 vim.api.nvim_create_autocmd("BufEnter", {
-	nested = true,
-	callback = function()
-		if #vim.api.nvim_list_wins() == 1 and vim.bo.filetype == "neo-tree" then
-			vim.cmd("quit")
-		end
-	end,
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and vim.bo.filetype == "neo-tree" then
+      vim.cmd("quit")
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { "*.config/dwl/*", "*.config/dwm/*" },
+  callback = function()
+    vim.bo.filetype = "c"
+  end,
 })
 
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-	pattern = { "*.hl", "hypr*.conf" },
-	callback = function(event)
-		print(string.format("starting hyprls for %s", vim.inspect(event)))
-		vim.lsp.start({
-			name = "hyprlang",
-			cmd = { "hyprls" },
-			root_dir = vim.fn.getcwd(),
-		})
-	end,
+  pattern = { "*.hl", "hypr*.conf" },
+  callback = function(event)
+    print(string.format("starting hyprls for %s", vim.inspect(event)))
+    vim.lsp.start({
+      name = "hyprlang",
+      cmd = { "hyprls" },
+      root_dir = vim.fn.getcwd(),
+    })
+  end,
 })
