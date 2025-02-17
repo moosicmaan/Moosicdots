b058b862f41cdf2e6246eff18b804db922748bfd
Jason Bradberry <j_bradberry@yahoo.com>
2025-02-17 11:24:29 -0700

    dwm rollback & script name issues

--- .config/dwm/config.def.h
+++ .config/dwm/config.def.h
@@ -45,6 +45,7 @@ static const char *tagsel[][2] = {
 };
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+// static const char *tags[] = { "➀", "➁", "➂", "➃", "➄", "➅", "➆", "➇", "➈" };
 
 static const Rule rules[] = {
 /* xprop(1):
