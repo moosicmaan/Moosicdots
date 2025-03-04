e05ae83f764c95defc8a35cb077096fc8d2096dd
Jason Bradberry <j_bradberry@yahoo.com>
2025-03-03 17:44:23 -0700

    org keybinds

--- .config/nvim/lua/plugins/org_mode.lua
+++ .config/nvim/lua/plugins/org_mode.lua
@@ -4,9 +4,52 @@ return {
   ft = { "org" },
   config = function()
     -- Setup orgmode
+    -- local org = require("orgmode")
+    -- org.setup_ts_grammar()
     require("orgmode").setup({
-      org_agenda_files = "/mnt/data/moosicmaan/AGENDA/orgfiles/**/*",
-      org_default_notes_file = "/mnt/data/moosicmaan/AGENDA/refile.org",
+      org_agenda_files = {
+        "/mnt/data/moosicmaan/ORG/agenda/*.org",
+        "/mnt/data/moosicmaan/ORG/*.org",
+      },
+      org_default_notes_file = "/mnt/data/moosicmaan/ORG/refile.org",
+      org_archive_location = "/mnt/data/moosicmaan/ORG/agenda/archive.org::* From %s",
+      win_split_mode = "vertical",
+      org_startup_folded = "content",
+      org_todo_keywords = { "TODO", "WAITING", "|", "DONE", "DELEGATED" },
+      org_todo_keyword_faces = {
+        WAITING = ":foreground blue :weight bold",
+        DELEGATED = ":background #FFFFFF :slant italic :underline on",
+        TODO = ":background #000000 :foreground red", -- overrides builtin color for `TODO` keyword
+      },
+      org_log_done = "note",
+      org_log_into_drawer = "LOGBOOK",
+      mappings = {
+        org = {
+          org_toggle_checkbox = { "<Leader>of", "g<Space>", desc = "Toggle Checkbox" },
+          org_todo = { "<Leader>o." },
+          org_todo_prev = { "<Leader>o/" },
+        },
+        global = {
+          -- org_capture = { "<D-c>", desc = "Open Capture Prompt" },
+          -- org_agenda = { "<Leader>oa" },
+          -- org_capture = { "<Leader>oc" },
+        },
+      },
+
+      org_capture_templates = {
+        r = {
+          description = "Repo",
+          template = "* [[%x][%(return string.match('%x', '([^/]+)$'))]]%?",
+          target = "/mnt/data/moosicmaan/ORG/repos.org",
+        },
+        {
+          j = {
+            description = "Journal",
+            template = "\n*** %<%Y-%m-%d> %<%A>\n**** %U\n\n%?",
+            target = "/mnt/data/moosicmaan/ORG/journal.org",
+          },
+        },
+      },
     })
   end,
 }
--- .config/nvim/lua/plugins/org_roam.lua
+++ .config/nvim/lua/plugins/org_roam.lua
@@ -1,21 +1,20 @@
 return {
-  "chipsenkbeil/org-roam.nvim",
-  tag = "0.1.1",
-  dependencies = {
-    {
-      "nvim-orgmode/orgmode",
-      tag = "0.3.7",
-    },
-  },
-  config = function()
-    require("org-roam").setup({
-      directory = "/mnt/data/moosicmaan/AGENDA/orgroam/**/*",
-      -- optional
-      org_files = {
-        "/mnt/data/moosicmaan/AGENDA/orgfiles/**/*",
-        "/mnt/data/moosicmaan/AGENDA/*.org",
-        "/mnt/data/moosicmaan/AGENDA/refile.org",
-      },
-    })
-  end,
+  -- "chipsenkbeil/org-roam.nvim",
+  -- tag = "0.1.1",
+  -- dependencies = {
+  --   {
+  --     "nvim-orgmode/orgmode",
+  --     tag = "0.3.7",
+  --   },
+  -- },
+  -- config = function()
+  --   require("org-roam").setup({
+  --     directory = "/mnt/data/moosicmaan/ORG/orgroam/",
+  --     -- optional
+  --     org_files = {
+  --       "/mnt/data/moosicmaan/ORG/agenda/*.org",
+  --       "/mnt/data/moosicmaan/ORG/*.org",
+  --     },
+  --   })
+  -- end,
 }
